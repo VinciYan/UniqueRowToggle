db7ff042ddadb147b6b2e315c0ca46bdbba26961
vinciyan <sywq6356010@163.com>
2024-09-09 14:40:04 +0800

    Init

---
+++ UniqueRowToggle/Form1.Designer.cs
@@ -0,0 +1,84 @@
+﻿namespace UniqueRowToggle
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            components = new System.ComponentModel.Container();
+            dockManager1 = new DevExpress.XtraBars.Docking.DockManager(components);
+            pathManagerDockPanel = new DevExpress.XtraBars.Docking.DockPanel();
+            dockPanel1_Container = new DevExpress.XtraBars.Docking.ControlContainer();
+            ((System.ComponentModel.ISupportInitialize)dockManager1).BeginInit();
+            pathManagerDockPanel.SuspendLayout();
+            SuspendLayout();
+            // 
+            // dockManager1
+            // 
+            dockManager1.Form = this;
+            dockManager1.RootPanels.AddRange(new DevExpress.XtraBars.Docking.DockPanel[] { pathManagerDockPanel });
+            dockManager1.TopZIndexControls.AddRange(new string[] { "DevExpress.XtraBars.BarDockControl", "DevExpress.XtraBars.StandaloneBarDockControl", "System.Windows.Forms.MenuStrip", "System.Windows.Forms.StatusStrip", "System.Windows.Forms.StatusBar", "DevExpress.XtraBars.Ribbon.RibbonStatusBar", "DevExpress.XtraBars.Ribbon.RibbonControl", "DevExpress.XtraBars.Navigation.OfficeNavigationBar", "DevExpress.XtraBars.Navigation.TileNavPane", "DevExpress.XtraBars.TabFormControl", "DevExpress.XtraBars.FluentDesignSystem.FluentDesignFormControl", "DevExpress.XtraBars.ToolbarForm.ToolbarFormControl" });
+            // 
+            // pathManagerDockPanel
+            // 
+            pathManagerDockPanel.Controls.Add(dockPanel1_Container);
+            pathManagerDockPanel.Dock = DevExpress.XtraBars.Docking.DockingStyle.Bottom;
+            pathManagerDockPanel.ID = new System.Guid("b265bcb4-8db2-4954-83b7-dd6b5bd256d9");
+            pathManagerDockPanel.Location = new System.Drawing.Point(0, 99);
+            pathManagerDockPanel.Name = "pathManagerDockPanel";
+            pathManagerDockPanel.OriginalSize = new System.Drawing.Size(200, 200);
+            pathManagerDockPanel.Size = new System.Drawing.Size(737, 200);
+            pathManagerDockPanel.Text = "路径选择";
+            // 
+            // dockPanel1_Container
+            // 
+            dockPanel1_Container.Location = new System.Drawing.Point(3, 27);
+            dockPanel1_Container.Name = "dockPanel1_Container";
+            dockPanel1_Container.Size = new System.Drawing.Size(731, 170);
+            dockPanel1_Container.TabIndex = 0;
+            // 
+            // Form1
+            // 
+            AutoScaleDimensions = new System.Drawing.SizeF(7F, 14F);
+            AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            ClientSize = new System.Drawing.Size(737, 299);
+            Controls.Add(pathManagerDockPanel);
+            Margin = new System.Windows.Forms.Padding(4, 3, 4, 3);
+            Name = "Form1";
+            Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)dockManager1).EndInit();
+            pathManagerDockPanel.ResumeLayout(false);
+            ResumeLayout(false);
+        }
+
+        #endregion
+
+        private DevExpress.XtraBars.Docking.DockManager dockManager1;
+        private DevExpress.XtraBars.Docking.DockPanel pathManagerDockPanel;
+        private DevExpress.XtraBars.Docking.ControlContainer dockPanel1_Container;
+    }
+}
+
---
+++ UniqueRowToggle/Form1.cs
@@ -0,0 +1,179 @@
+﻿using DevExpress.XtraGrid;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Windows.Forms;
+using System.ComponentModel.DataAnnotations;
+using DevExpress.Utils.Extensions;
+
+namespace UniqueRowToggle
+{
+    public partial class Form1 : DevExpress.XtraEditors.XtraForm
+    {
+        public Form1()
+        {
+            InitializeComponent();
+            CreatePathDockPanel();
+        }
+        public class DataHelper
+        {
+            public static string[] companies = new string[] { "Hanari Carnes", "Que Delícia", "Romero y tomillo", "Mère Paillarde",
+            "Comércio Mineiro", "Reggiani Caseifici", "Maison Dewey" };
+            public static BindingList<Record> GetData(int count)
+            {
+                BindingList<Record> records = new BindingList<Record>();
+                Random rnd = new Random();
+                for (int i = 0; i < count; i++)
+                {
+                    int n = rnd.Next(10);
+                    records.Add(new Record()
+                    {
+                        ID = i + 100,
+                        CompanyName = companies[i % companies.Length],
+                        RequiredDate = DateTime.Today.AddDays(n - 5),
+                        Value = i % 2 == 0 ? (i + 1) * 123 : i * 231
+                    });
+                };
+                return records;
+            }
+        }
+        public class Record : INotifyPropertyChanged
+        {
+            public Record()
+            {
+            }
+            int id;
+            public int ID
+            {
+                get { return id; }
+                set
+                {
+                    if (id != value)
+                    {
+                        id = value;
+                        OnPropertyChanged();
+                    }
+                }
+            }
+
+            string text;
+            [DisplayName("Company")]
+            public string CompanyName
+            {
+                get { return text; }
+                set
+                {
+                    if (text != value)
+                    {
+                        if (string.IsNullOrEmpty(value))
+                            throw new Exception();
+                        text = value;
+                        OnPropertyChanged();
+                    }
+                }
+            }
+            Nullable<decimal> val;
+            [DataType(System.ComponentModel.DataAnnotations.DataType.Currency)]
+            [DisplayName("Payment")]
+            public Nullable<decimal> Value
+            {
+                get { return val; }
+                set
+                {
+                    if (val != value)
+                    {
+                        val = value;
+                        OnPropertyChanged();
+                    }
+                }
+            }
+            DateTime dt;
+            [DisplayFormat(DataFormatString = "d")]
+            public DateTime RequiredDate
+            {
+                get { return dt; }
+                set
+                {
+                    if (dt != value)
+                    {
+                        dt = value;
+                        OnPropertyChanged();
+                    }
+                }
+            }
+            bool state;
+            public bool Processed
+            {
+                get { return state; }
+                set
+                {
+                    if (state != value)
+                    {
+                        state = value;
+                        OnPropertyChanged();
+                    }
+                }
+            }
+
+            public override string ToString()
+            {
+                return string.Format("ID = {0}, Text = {1}", ID, CompanyName);
+            }
+
+            public event PropertyChangedEventHandler PropertyChanged;
+            protected void OnPropertyChanged([CallerMemberName] string propertyName = "")
+            {
+                if (PropertyChanged != null)
+                    PropertyChanged(this, new PropertyChangedEventArgs(propertyName));
+            }
+        }
+        private void CreatePathDockPanel()
+        {
+            GridControl gridControl = new GridControl()
+            {
+                Name = "gridControl1",
+                Dock = DockStyle.Fill
+            };
+            gridControl.DataSource = DataHelper.GetData(30);
+            this.pathManagerDockPanel.ControlContainer.AddControl(gridControl);
+
+            // 创建 GridView
+            DevExpress.XtraGrid.Views.Grid.GridView gridView = new DevExpress.XtraGrid.Views.Grid.GridView(gridControl);
+            gridControl.MainView = gridView;
+            gridView.CellValueChanging += GridView_CellValueChanging;
+        }
+        private void GridView_CellValueChanging(object sender, DevExpress.XtraGrid.Views.Base.CellValueChangedEventArgs e)
+        {
+
+            if (e.Column.FieldName == "Processed")
+            {
+                DevExpress.XtraGrid.Views.Grid.GridView view = sender as DevExpress.XtraGrid.Views.Grid.GridView;
+                if (view == null) return;
+                bool newValue = (bool)e.Value;
+                if (newValue)
+                {
+                    // 遍历所有行，取消当前行之外的所有行的勾选
+                    for (int i = 0; i < view.RowCount; i++)
+                    {
+                        if (i != e.RowHandle)
+                        {
+                            view.SetRowCellValue(i, "Processed", false);
+                        }
+                    }
+                }
+                else
+                {
+                    // 如果用户试图取消选中，我们阻止这个操作
+                    view.SetRowCellValue(e.RowHandle, "Processed", true);
+                }
+                // 刷新视图以显示更改
+                view.RefreshData();
+            }
+        }
+    }
+}
